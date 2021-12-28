ad1c1ed8a9d36e28bd5ae58649307a93b3e4d596
Achal Jhawar <35405812+achaljhawar@users.noreply.github.com>
2021-12-28 23:18:28 +0530

    Added details of team members

--- src/pages/Team/team-members.js
+++ src/pages/Team/team-members.js
@@ -1,32 +1,32 @@
 export const members = [
   {
-    name: "Lorem Ipsum",
+    name: "Achal Jhawar",
+    img: "https://i.imgur.com/PaEFtAh.jpg",
+    designation: "Director",
+  },
+  {
+    name: "Vihaan Singh",
     img: "https://avatars.githubusercontent.com/u/64571343?s=400&u=333faad5070566e4b0a2b436bc0e5f4187f5607b&v=4",
     designation: "Head Code",
   },
   {
-    name: "Lorem Ipsum",
-    img: "https://thumbs2.imgbox.com/d1/bd/mbZZabom_t.jpg",
-    designation: "Designation",
+    name: "Kartik Saraf",
+    img: "https://i.imgur.com/Nm0sQXy.jpg",
+    designation: "Head Gaming",
   },
   {
-    name: "Lorem Ipsum",
-    img: "https://thumbs2.imgbox.com/d1/bd/mbZZabom_t.jpg",
-    designation: "Designation",
+    name: "Shravani Bhate",
+    img: "https://i.imgur.com/eHFzXS6.jpg",
+    designation: "Head Discussion ",
   },
   {
-    name: "Lorem Ipsum",
-    img: "https://thumbs2.imgbox.com/d1/bd/mbZZabom_t.jpg",
-    designation: "Designation",
+    name: "Rushil Jain",
+    img: "https://i.imgur.com/DSKlTCz.jpg",
+    designation: "Head Quiz",
   },
   {
-    name: "Lorem Ipsum",
-    img: "https://thumbs2.imgbox.com/d1/bd/mbZZabom_t.jpg",
-    designation: "Designation",
-  },
-  {
-    name: "Lorem Ipsum",
-    img: "https://thumbs2.imgbox.com/d1/bd/mbZZabom_t.jpg",
-    designation: "Designation",
+    name: "Anvi Murthy",
+    img: "https://i.imgur.com/CGuvPPF.jpg",
+    designation: "Head Writing",
   }
 ];
