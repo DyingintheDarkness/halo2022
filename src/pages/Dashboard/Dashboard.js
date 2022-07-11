f294bdba3a75a8dc949ba0367dea0c20d782d84d
Maitreya29 <maitreyapatni30@gmail.com>
2022-07-11 09:50:42 +0530

    Halocon: rework team and resources

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -18,15 +18,9 @@ const Dashboard = () => {
   const [signInStatus, setSignInStatus] = useSharedState(signInStatusAtom);
   const [selectedEvents, setSelectedEvents] =
     useSharedState(selectedEventsAtom);
-  const [redirect, setRedirect] = useSharedState(redirectAtom)
+  const [redirect, setRedirect] = useSharedState(redirectAtom);
   const [viewPrompts, setViewPrompts] = useState(false);
-  const events = [
-    "coding",
-    "designing",
-    "discussion",
-    "gaming",
-    "quiz",
-  ];
+  const events = ["coding", "designing", "discussion", "gaming", "quiz"];
 
   useEffect(() => {
     const current = new Date().getTime();
@@ -41,13 +35,12 @@ const Dashboard = () => {
       {signInStatus && user !== null && selectedEvents !== null ? (
         <Layout>
           <div className="mt-12 relative z-30 mb-10">
-
             <h1 className="text-center font-sarala font-bold xsm:text-xl md:text-2xl lg:text-4xl text-lg">
-              Hi, {user.name}. Welcome to the club!<br />
+              Hi, {user.name}. Welcome to the club!
+              <br />
               Glad to have you here!
             </h1>
             <div className="flex flex-col items-center justify-center mt-5">
-
               <div className="flex flex-col xsm:flex-row xsm:flex-wrap justify-center items-center lg:w-10/12 xl:w-8/12">
                 {events.map((event, i) => (
                   <EventCard
@@ -57,59 +50,74 @@ const Dashboard = () => {
                   />
                 ))}
               </div>
-              <button className="bg-green_1 font-roboto w-20 rounded-md h-7  " onClick={() => updateEvents(selectedEvents, toast)}>update</button>
+              <button
+                className="bg-green_1 font-roboto w-20 rounded-md h-7  "
+                onClick={() => updateEvents(selectedEvents, toast)}
+              >
+                update
+              </button>
             </div>
 
-
-
             <div className="flex flex-col gap-2 mt-10 ml-2">
-              <h1 className="font-pro text-xl xsm:text-2xl lg:text-3xl">For any issues, please refer to the instructions below:-</h1>
+              <h1 className="font-pro text-xl xsm:text-2xl lg:text-3xl">
+                For any issues, please refer to the instructions below:-
+              </h1>
               <ul className="instructions font-sarabun text-grey_1 pl-5 list-decimal xsm:text-lg">
-                <li>Just click and select the event boxes, whichever events you are interested in and then just click on update.</li>
-                <li>If you have any further problems related to something,<br />
-                  please do not hesitate to contact us and we will be ready to help you out.</li>
-                <li>{!viewPrompts ? "Prompts are now available just click on the View Prompts Button Below" : "Information for the prompts will be available later on."}</li>
-                <li>As for any new updates, keep an eye out for announcements on our <a className="text-blue_2" href="https://discord.gg/2TB4RJr4U7" rel="noreferrer noopener"> halolegion’s Discord Server</a>.</li>
+                <li>
+                  Just click and select the event boxes, whichever events you
+                  are interested in and then just click on update.
+                </li>
+                <li>
+                  If you have any further problems related to something,
+                  <br />
+                  please do not hesitate to contact us and we will be ready to
+                  help you out.
+                </li>
+                <li>
+                  {!viewPrompts
+                    ? "Prompts are now available just click on the View Prompts Button Below"
+                    : "Information for the prompts will be available later on."}
+                </li>
+                <li>
+                  As for any new updates, keep an eye out for announcements on
+                  our{" "}
+                  <a
+                    className="text-blue_2"
+                    href="https://discord.gg/2TB4RJr4U7"
+                    rel="noreferrer noopener"
+                  >
+                    {" "}
+                    halolegion’s Discord Server
+                  </a>
+                  .
+                </li>
                 <li>Besides, have a great time.</li>
               </ul>
             </div>
 
-
-{viewPrompts ?
-
-<div className="mt-10 flex justify-center items-center">
-
-            <div className="flex w-80 xsm:w-8/12 lg:w-6/12 xsm:h-40 sm:h-56 sm:text-3xl items-center justify-center text-center h-36 bg-purple_1 font-sarala font-bold text-white text-2xl shadow-spread-md rounded-md">
-                <a
-                  href="https://docs.google.com/document/d/1txBB--TMTfyLM5E_YWxDeU0u3re0lVP2e9XD3gcaYCY/edit?usp=sharing"
-                  target="_blank"
-                  rel="noreferrer"
-                  className=""
+            {viewPrompts ? (
+              <div className="mt-10 flex justify-center items-center">
+                <div className="flex w-80 xsm:w-8/12 lg:w-6/12 xsm:h-40 sm:h-56 sm:text-3xl items-center justify-center text-center h-36 bg-purple_1 font-sarala font-bold text-white text-2xl shadow-spread-md rounded-md">
+                  <a
+                    href="https://docs.google.com/document/d/1txBB--TMTfyLM5E_YWxDeU0u3re0lVP2e9XD3gcaYCY/edit?usp=sharing"
+                    target="_blank"
+                    rel="noreferrer"
+                    className=""
                   >
-                  View Prompts
-                </a>
-                  </div>
-                    </div>
-          
-                  : ""}
+                    View Prompts
+                  </a>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
 
-
-
-<div className="mt-20">
-
-</div>
-
-
-
-
-
+            <div className="mt-20"></div>
           </div>
-
         </Layout>
       ) : (
         <Redirect to={redirect} />
-      )
-      }
+      )}
     </>
   );
 };
--- src/pages/Resources/Resources.js
+++ src/pages/Resources/Resources.js
@@ -6,22 +6,15 @@ const Resources = () => {
   return (
     <Layout>
 <div className="mt-10 mb-10">
-  <h1 className="font-sarala font-bold text-lg xsm:text-2xl sm:text-4xl text-center">You can learn and develop your<br/>skills through these resources</h1>
 
-<h2 className="mt-10 font-pro text-center break-words xsm:text-lg sm:text-xl md:text-2xl">
-Click on the links below to discover the resources
-</h2>
-
-
-
-<div className="bg-white h-52 xsm:h-56 flex items-center justify-center flex-col gap-3 mt-5">
-  <h1 className="font-sansation font-bold text-xl -mt-10 xsm:text-2xl xsm:-mt-5 pt-10">
-  Resources Links:-
+<div className="bg-card flex px-14 flex-col gap-3 mt-10">
+<div className="flex flex-col xsm:text-lg pt-10 font-sarabun pb-10">
+<h1 className="font-sarabun text-white text-xl pl-10 pb-10">
+  Click on the links below to discover learning resources
   </h1>
 
-<div className="underline flex flex-col xsm:text-lg text-blue_3 gap-1 font-montserrat font-bold pb-10">
 {resources.map(res => {
-  return <a href={res.link}>{res.name}</a>
+  return <a className="resCard underline py-10 text-xl" href={res.link}>{res.name}</a>
 })}
 </div>
 
