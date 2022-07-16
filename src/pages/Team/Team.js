d5944ef642ebce4f2915e2d465241bbe38e82c0f
achaljhawar <achaldps@gmail.com>
2022-07-16 19:57:33 +0530

    changing designations on the main site

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -9,21 +9,50 @@ const Team = () => {
           <div className="flex flex-col justify-center items-center xsm:flex-row xsm:flex-wrap gap-10 lg:ml-1">
             {members.map((member) => {
               if (member.instagram === "empty") {
-                return(
-                  <div
-                    key={member.name}
-                    className={`rounded-md about-card items-center bg-card w-60 h-full pb-1`}
-                  >
-                    <div className="flex flex-col items-center centershit">
-                      <h1 className="font-pro font-medium xsm:text-lg md:text-xl VertMargTop">
-                        {member.name}
-                      </h1>
-                      <h2 className="font-segoe font-semibold text-xsm lg:text-lg VertMarg10">
-                        {member.designation}
-                      </h2>
+                if (member.twitter === "empty") {
+                  return(
+                    <div
+                      key={member.name}
+                      className={`rounded-md about-card items-center bg-card w-60 h-full pb-1`}
+                    >
+                      <div className="flex flex-col items-center centershit">
+                        <h1 className="font-pro font-medium xsm:text-lg md:text-xl VertMargTop">
+                          {member.name}
+                        </h1>
+                        <h2 className="font-segoe font-semibold text-xsm lg:text-lg VertMarg10">
+                          {member.designation}
+                        </h2>
+                      </div>
                     </div>
-                  </div>
-                );
+                  );
+                } else {
+                  return(
+                    <div
+                      key={member.name}
+                      className={`rounded-md about-card items-center bg-card w-60 h-full pb-1`}
+                    >
+                      <div className="flex flex-col items-center centershit">
+                        <h1 className="font-pro font-medium xsm:text-lg md:text-xl VertMargTop">
+                          {member.name}
+                        </h1>
+                        <h2 className="font-segoe font-semibold text-xsm lg:text-lg VertMargTop">
+                          {member.designation}
+                        </h2>
+                        <div className="VertMarg10 flex flex-row items-center">
+                          <a href={member.twitter}>
+                            <svg
+                              className="w-6 h-6"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 24 24"
+                            >
+                              <path fill="#9EA6EC" d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 005.001-1.721 4.036 4.036 0 01-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 01-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 01-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 008.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 014.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 002.556-.973 4.02 4.02 0 01-1.771 2.22 8.073 8.073 0 002.319-.624 8.645 8.645 0 01-2.019 2.083z"></path>
+                            </svg>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                }
               } else if (member.github === "empty") {
                 return (
                   <div
