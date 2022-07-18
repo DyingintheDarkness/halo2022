949cd93198aaf4f55197b1a080e0f5eab2d0da10
achaljhawar <achaldps@gmail.com>
2022-07-18 15:15:53 +0530

    changed loading text color to white

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loading = () => {
     return (
-        <div className='flex items-center justify-center h-screen font-medium text-4xl md:text-5xl text-current'>
-            <h1>Loading...</h1>
+        <div className='flex items-center justify-center h-screen font-medium text-4xl md:text-5xl'>
+            <h1 style="color:white;">Loading...</h1>
         </div>
     )
 }
