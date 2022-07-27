9597ca57db0a148d00492c1e5b2ce692bdd64d05
Maitreya29 <maitreyapatni30@gmail.com>
2022-07-27 10:25:50 +0530

    halo2022: fix laoding screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
     return (
-        <div className='flex items-center justify-center h-screen font-medium text-4xl md:text-5xl'>
+        <div className='flex items-center text-white justify-center h-screen  font-medium text-4xl md:text-5xl'>
             <h1>Loading...</h1>
         </div>
     )
